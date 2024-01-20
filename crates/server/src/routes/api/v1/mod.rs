83628a92836ccb28ea70e2823042337d0fb7154e
Mathieu Hardy <mathieu.hardy@devialet.com>
2024-01-20 18:27:30 +0100

    feat: add /api prefix in URLs

---
+++ crates/server/src/routes/api/v1/mod.rs
@@ -0,0 +1,3 @@
+//! This file contains all routes dedicated to version 1 of the APIs.
+
+pub mod users;
--- crates/server/tests/users.rs
+++ crates/server/tests/users.rs
@@ -4,7 +4,7 @@ use test_utils::init_server;
 async fn me() {
     let client = init_server().await.unwrap();
 
-    let response = client.get("/users/me").send().await.unwrap();
+    let response = client.get("/api/users/me").send().await.unwrap();
     assert_eq!(response.status(), test_utils::StatusCode::OK);
 
     let body = response.text().await.unwrap();
