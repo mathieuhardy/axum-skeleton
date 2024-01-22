e171cd6518a5f1eaec49201fc5a1ca26c3ef89d8
Mathieu Hardy <mathieu.hardy@devialet.com>
2024-01-22 19:39:33 +0100

    chore: add trigger for updated_at columns

--- crates/database/src/requests.rs
+++ crates/database/src/requests.rs
@@ -10,14 +10,3 @@ where
     ($2 is null or $2 = email)
 order by
     name asc;";
-
-/// Undocumented.
-pub const SQL_USERS_FIND_BY_NAME: &str = "select
-    *
-from
-    users
-where
-    ($1 is null or $1 = name) and
-    ($2 is null or $2 = email)
-order by
-    name asc;";
