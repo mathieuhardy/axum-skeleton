af2f2ef3b84ebd95bb53302b306a948869b18bff
Mathieu Hardy <mathieu.hardy@climateseed.com>
2024-01-25 20:14:51 +0100

    feat: add setup and teardown callbacks for tests

--- crates/server/tests/users.rs
+++ crates/server/tests/users.rs
@@ -1,8 +1,12 @@
 use serial_test::serial;
-use test_utils::init_server;
+use test_utils::*;
 use urlencoding::encode;
 
 use database::models::users::User;
+
+async fn setup() -> TestClient {
+    init_server().await.unwrap()
+}
 
 mod get {
     use super::*;
@@ -10,54 +14,68 @@ mod get {
     #[tokio::test]
     #[serial]
     async fn me() {
-        let client = init_server().await.unwrap();
+        run_test(
+            setup,
+            |client| async move {
+                let client = client.lock().unwrap();
 
-        let response = client.get("/api/users/me").send().await.unwrap();
-        assert_eq!(response.status(), test_utils::StatusCode::OK);
+                let response = client.get("/api/users/me").send().await.unwrap();
+                assert_eq!(response.status(), test_utils::StatusCode::OK);
 
-        let user = response.json::<User>().await.unwrap();
-        assert_eq!(user.name, "John Doe");
-        assert_eq!(user.email, "john@doe.com");
+                let user = response.json::<User>().await.unwrap();
+                assert_eq!(user.name, "John Doe");
+                assert_eq!(user.email, "john@doe.com");
+            },
+            no_teardown,
+        )
+        .await;
     }
 
     #[tokio::test]
     #[serial]
     async fn by_filters() {
-        let client = init_server().await.unwrap();
+        run_test(
+            setup,
+            |client| async move {
+                let client = client.lock().unwrap();
 
-        // By name
-        let response = client
-            .get(format!("/api/users?name={}", encode("John Doe")))
-            .send()
-            .await
-            .unwrap();
-        assert_eq!(response.status(), test_utils::StatusCode::OK);
+                // By name
+                let response = client
+                    .get(format!("/api/users?name={}", encode("John Doe")))
+                    .send()
+                    .await
+                    .unwrap();
+                assert_eq!(response.status(), test_utils::StatusCode::OK);
 
-        let users = response.json::<Vec<User>>().await.unwrap();
-        assert_eq!(users.len(), 1);
-        assert_eq!(users[0].name, "John Doe");
-        assert_eq!(users[0].email, "john@doe.com");
+                let users = response.json::<Vec<User>>().await.unwrap();
+                assert_eq!(users.len(), 1);
+                assert_eq!(users[0].name, "John Doe");
+                assert_eq!(users[0].email, "john@doe.com");
 
-        // By name (not found)
-        let response = client.get("/api/users?name=404").send().await.unwrap();
-        assert_eq!(response.status(), test_utils::StatusCode::NOT_FOUND);
+                // By name (not found)
+                let response = client.get("/api/users?name=404").send().await.unwrap();
+                assert_eq!(response.status(), test_utils::StatusCode::NOT_FOUND);
 
-        // By email
-        let response = client
-            .get("/api/users?email=john@doe.com")
-            .send()
-            .await
-            .unwrap();
-        assert_eq!(response.status(), test_utils::StatusCode::OK);
+                // By email
+                let response = client
+                    .get("/api/users?email=john@doe.com")
+                    .send()
+                    .await
+                    .unwrap();
+                assert_eq!(response.status(), test_utils::StatusCode::OK);
 
-        let users = response.json::<Vec<User>>().await.unwrap();
-        assert_eq!(users.len(), 1);
-        assert_eq!(users[0].name, "John Doe");
-        assert_eq!(users[0].email, "john@doe.com");
+                let users = response.json::<Vec<User>>().await.unwrap();
+                assert_eq!(users.len(), 1);
+                assert_eq!(users[0].name, "John Doe");
+                assert_eq!(users[0].email, "john@doe.com");
 
-        // By email (not found)
-        let response = client.get("/api/users?email=404").send().await.unwrap();
-        assert_eq!(response.status(), test_utils::StatusCode::NOT_FOUND);
+                // By email (not found)
+                let response = client.get("/api/users?email=404").send().await.unwrap();
+                assert_eq!(response.status(), test_utils::StatusCode::NOT_FOUND);
+            },
+            no_teardown,
+        )
+        .await;
     }
 }
 
@@ -67,34 +85,48 @@ mod post {
     #[tokio::test]
     #[serial]
     async fn create_from_form() {
-        let client = init_server().await.unwrap();
+        run_test(
+            setup,
+            |client| async move {
+                let client = client.lock().unwrap();
 
-        let user = [("name", "John Doe"), ("email", "john@doe.com")];
+                let user = [("name", "John Doe"), ("email", "john@doe.com")];
 
-        let response = client.post("/api/users").form(&user).send().await.unwrap();
-        assert_eq!(response.status(), test_utils::StatusCode::OK);
+                let response = client.post("/api/users").form(&user).send().await.unwrap();
+                assert_eq!(response.status(), test_utils::StatusCode::OK);
 
-        let user = response.json::<User>().await.unwrap();
-        assert_eq!(user.name, "John Doe");
-        assert_eq!(user.email, "john@doe.com");
+                let user = response.json::<User>().await.unwrap();
+                assert_eq!(user.name, "John Doe");
+                assert_eq!(user.email, "john@doe.com");
+            },
+            no_teardown,
+        )
+        .await;
     }
 
     #[tokio::test]
     #[serial]
     async fn create_from_json() {
-        let client = init_server().await.unwrap();
+        run_test(
+            setup,
+            |client| async move {
+                let client = client.lock().unwrap();
 
-        let user = User {
-            name: "John Doe".to_string(),
-            email: "john@doe.com".to_string(),
-            ..User::default()
-        };
+                let user = User {
+                    name: "John Doe".to_string(),
+                    email: "john@doe.com".to_string(),
+                    ..User::default()
+                };
 
-        let response = client.post("/api/users").json(&user).send().await.unwrap();
-        assert_eq!(response.status(), test_utils::StatusCode::OK);
+                let response = client.post("/api/users").json(&user).send().await.unwrap();
+                assert_eq!(response.status(), test_utils::StatusCode::OK);
 
-        let user = response.json::<User>().await.unwrap();
-        assert_eq!(user.name, "John Doe");
-        assert_eq!(user.email, "john@doe.com");
+                let user = response.json::<User>().await.unwrap();
+                assert_eq!(user.name, "John Doe");
+                assert_eq!(user.email, "john@doe.com");
+            },
+            no_teardown,
+        )
+        .await;
     }
 }
