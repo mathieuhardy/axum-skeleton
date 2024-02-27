fe5e30ad37e20e01683f64d403789119709255a4
Mathieu Hardy <mhardy2008@gmail.com>
2024-02-27 19:11:58 +0100

    chore: add default entries in database

--- migrations/20240117201401_add_users_table.up.sql
+++ migrations/20240117201401_add_users_table.up.sql
@@ -1,4 +1,12 @@
+-- Create needed extensions
+
 create extension if not exists "uuid-ossp";
+
+-- Create functions
+
+-- TODO: updated_at
+
+-- Create tables
 
 create table users (
     id         uuid primary key default uuid_generate_v4(),
@@ -8,4 +16,11 @@ create table users (
     updated_at timestamp with time zone not null default now(),
 
     unique(email)
-)
+);
+
+-- For testing purpose
+
+insert into users (name, email)
+values
+    ('John Doe', 'john@doe.com'),
+    ('Jane Doe', 'jane@doe.com');
