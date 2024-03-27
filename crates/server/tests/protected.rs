a43a1071d8d6866633a0a81c35b873fa472dff91
Mathieu Hardy <mhardy2008@gmail.com>
2024-03-27 21:55:47 +0100

    fix: set back dedicated unit tests functions

--- crates/server/tests/protected.rs
+++ crates/server/tests/protected.rs
@@ -16,64 +16,66 @@ async fn setup() -> TestClient {
     init_server().await.unwrap()
 }
 
-#[hook(setup, _)]
-#[tokio::test]
-#[serial]
-async fn all() {
-    |client| async move {
-        let client = client.lock().await;
-
-        get::unauthorized(&client).await;
-        get::after_login(&client).await;
-        get::after_logout(&client).await;
-    }
-}
-
 mod get {
     use super::*;
 
-    pub async fn unauthorized(client: &TestClient) {
-        println!("{}::unauthorized", module_path!());
+    #[hook(setup, _)]
+    #[tokio::test]
+    #[serial]
+    pub async fn unauthorized() {
+        |client| async move {
+            let client = client.lock().await;
 
-        let response = client.get("/protected").send().await;
-        assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+            let response = client.get("/protected").send().await;
+            assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+        }
     }
 
-    pub async fn after_login(client: &TestClient) {
-        println!("{}::after_login", module_path!());
+    #[hook(setup, _)]
+    #[tokio::test]
+    #[serial]
+    pub async fn after_login() {
+        |client| async move {
+            let client = client.lock().await;
 
-        // Login
-        auth::post::test_login(
-            client,
-            auth::DataType::Json,
-            EmailValidity::Valid,
-            PasswordValidity::Valid,
-        )
-        .await;
+            // Login
+            auth::post::test_login(
+                &client,
+                auth::DataType::Json,
+                EmailValidity::Valid,
+                PasswordValidity::Valid,
+            )
+            .await;
 
-        // Check access after login (must be successful)
-        let response = client.get("/protected").send().await;
-        assert_eq!(response.status(), StatusCode::OK);
+            // Check access after login (must be successful)
+            let response = client.get("/protected").send().await;
+            assert_eq!(response.status(), StatusCode::OK);
+        }
     }
 
-    pub async fn after_logout(client: &TestClient) {
-        println!("{}::after_logout", module_path!());
+    #[hook(setup, _)]
+    #[tokio::test]
+    #[serial]
+    pub async fn after_logout() {
+        |client| async move {
+            let client = client.lock().await;
 
-        // Login
-        auth::post::test_login(
-            client,
-            auth::DataType::Json,
-            EmailValidity::Valid,
-            PasswordValidity::Valid,
-        )
-        .await;
+            // Login
+            auth::post::test_login(
+                &client,
+                auth::DataType::Json,
+                EmailValidity::Valid,
+                PasswordValidity::Valid,
+            )
+            .await;
 
-        // Logout
-        let response = client.post("/logout").send().await;
-        assert_eq!(response.status(), StatusCode::OK);
+            // Logout
+            let response = client.post("/logout").send().await;
+            assert_eq!(response.status(), StatusCode::OK);
 
-        // Check access after login (must be successful)
-        let response = client.get("/protected").send().await;
-        assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+            // Check access after login (must be successful)
+            let response = client.get("/protected").send().await;
+            assert_eq!(response.status(), StatusCode::UNAUTHORIZED);
+        }
     }
 }
