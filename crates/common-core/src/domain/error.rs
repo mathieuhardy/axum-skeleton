4a138a81702afe6ca0a2732b736066958760b27f
Mathieu Hardy <mhardy2008@gmail.com>
2025-04-16 19:35:39 +0200

    chore: add comments to common-core crate

--- crates/common-core/src/domain/error.rs
+++ crates/common-core/src/domain/error.rs
@@ -4,13 +4,26 @@ use axum::response::{IntoResponse, Response};
 use axum::Json;
 use serde::Serialize;
 
+/// A structure used to return an error to the frontend(s).
 #[derive(Debug, Serialize)]
 pub struct ApiError {
+    /// A unique string identifying the error and that can be used to fetch a message translated in
+    /// the user's language.
     pub code: &'static str,
+
+    /// A human-readable message describing the error in english.
     pub message: String,
 }
 
 impl ApiError {
+    /// Creates a new `ApiError` instance.
+    ///
+    /// # Arguments
+    /// * `code` - A unique string identifying the error.
+    /// * `message` - A human-readable message describing the error in english.
+    ///
+    /// # Returns
+    /// A new `ApiError` instance.
     pub fn new(code: &'static str, message: String) -> Self {
         Self { code, message }
     }
--- crates/common-core/src/domain/mod.rs
+++ crates/common-core/src/domain/mod.rs
@@ -1,5 +1,5 @@
 //! List of all entities used in this crate and eventually by other crates.
 
-pub mod error;
-pub mod state;
-pub mod use_case;
+pub(crate) mod error;
+pub(crate) mod state;
+pub(crate) mod use_case;
